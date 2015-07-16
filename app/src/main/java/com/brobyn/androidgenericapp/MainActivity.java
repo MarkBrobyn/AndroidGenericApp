78bd602b4b888cc52b9892280e7bfb2edab0cd54
mark22 <markbrobyn@yahoo.co.uk>
2015-07-16 22:21:14 +0100

    added button listeners

--- app/src/main/java/com/brobyn/androidgenericapp/MainActivity.java
+++ app/src/main/java/com/brobyn/androidgenericapp/MainActivity.java
@@ -5,19 +5,42 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     TextView status;
+    Button button_1;
+    Button button_2;
+    Button button_3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         status=(TextView) findViewById(R.id.status);
-        status.setText("Waiting");
+        status.setText("Waiting...");
+
+        button_1=(Button)findViewById(R.id.button_1);
+        button_1.setOnClickListener(this);
+
+        button_2=(Button)findViewById(R.id.button_2);
+        button_2.setOnClickListener(this);
+
+        button_3=(Button)findViewById(R.id.button_3);
+        button_3.setOnClickListener(this);
+
+        Button button_4=(Button)findViewById(R.id.button_4);
+        button_4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                status.setText("Button 4 clicked");
+            }
+        });
     }
 
     @Override
@@ -26,6 +49,25 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    /*
+     public class MyActivity extends Activity {
+     protected void onCreate(Bundle icicle) {
+         super.onCreate(icicle);
+
+         setContentView(R.layout.content_layout_id);
+
+         final Button button = (Button) findViewById(R.id.button_id);
+         button.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+                 // Perform action on click
+             }
+         });
+     }
+ }
+
+    */
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -40,5 +82,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        status.setText("Button 1 clicked");
     }
 }
