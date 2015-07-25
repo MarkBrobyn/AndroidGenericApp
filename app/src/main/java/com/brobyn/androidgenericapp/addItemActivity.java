c9a89407551412b82597080251d405cdae74d50a
mark22 <markbrobyn@yahoo.co.uk>
2015-07-25 22:28:42 +0100

    list view working in add item...

--- app/src/main/java/com/brobyn/androidgenericapp/addItemActivity.java
+++ app/src/main/java/com/brobyn/androidgenericapp/addItemActivity.java
@@ -5,16 +5,39 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 // http://www.mysamplecode.com/2011/10/android-dynamic-layout-using-xml-add.html
 // http://stackoverflow.com/questions/4878159/android-whats-the-best-way-to-share-data-between-activities
 
 public class addItemActivity extends ActionBarActivity {
 
+    ListView myListView;
+    ArrayAdapter myArrayAdapter;
+    ArrayList<String> myArrayList=new ArrayList<String>();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_item_activity);
+
+        myArrayList.add("Mark");
+        myArrayList.add("Gaby");
+        myArrayList.add("Izzy");
+        myArrayList.add("Paul");
+        myArrayList.add("Viki");
+
+        myListView=(ListView) findViewById(R.id.additem_listview);
+        myArrayAdapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                myArrayList);
+        myListView.setAdapter(myArrayAdapter);
+        myArrayAdapter.notifyDataSetChanged();
     }
 
     @Override
